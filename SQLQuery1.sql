45562930e898a36abbac9c29f64f16a0c9bb2598
Arivunithi Shanmugavel <arivunithishanmugavel@outlook.com>
2021-12-27 08:59:45 +0530

    Add project files.

---
+++ SQLQuery1.sql
@@ -0,0 +1,5 @@
+
+SELECT * FROM COURSE
+select * from Enrollment
+INSERT INTO COURSE ("courseid","Credits","Title") VALUES (112,3,'Maths')
+INSERT INTO Enrollment ("CourseID","Grade","StudentID") values (112,'1',1)
